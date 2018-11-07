82bf66ed9e2fa339ad53c7e5e03dc36c73770d10
jnyao <jienan.yao@mail.utoronto.ca>
2018-11-07 16:22:50 -0500

    Bone first try

--- main.cpp
+++ main.cpp
@@ -2,18 +2,24 @@
 #include "euler_characteristic.h"
 #include <igl/read_triangle_mesh.h>
 #include <igl/opengl/glfw/Viewer.h>
+#include "generate_bone.h"
+#include <iostream>
 
 int main(int argc, char *argv[])
 {
   Eigen::MatrixXd V;
   Eigen::MatrixXi F;
-  // Load in a mesh
-  igl::read_triangle_mesh(argc>1 ? argv[1] : "../shared/data/bunny.off", V, F);
+//  // Load in a mesh
+//  igl::read_triangle_mesh(argc>1 ? argv[1] : "../shared/data/bunny.off", V, F);
+//
+//  Eigen::MatrixXi E = edges(F);
+//  int Chi = euler_characteristic(F);
+//  std::cout<<"Edge list E is "<<E.rows()<<"x"<<E.cols()<<std::endl;
+//  std::cout<<"Euler Characteristic: "<<Chi<<std::endl;
 
-  Eigen::MatrixXi E = edges(F);
-  int Chi = euler_characteristic(F);
-  std::cout<<"Edge list E is "<<E.rows()<<"x"<<E.cols()<<std::endl;
-  std::cout<<"Euler Characteristic: "<<Chi<<std::endl;
+  generate_bone(Eigen::Vector3d(0, 0, 0), Eigen::Vector3d(0, 0, 5), V, F);
+
+  std::cout << F << std::endl;
 
   // Create a libigl Viewer object 
   igl::opengl::glfw::Viewer viewer;
--- src/generate_bone.cpp
+++ src/generate_bone.cpp
@@ -1,17 +1,41 @@
 #include "generate_bone.h"
+#include <Eigen/Geometry>
 
 void generate_bone(const Eigen::Vector3d &p0,
                    const Eigen::Vector3d &p1,
                    Eigen::MatrixXd &V,
                    Eigen::MatrixXi &F) {
   // Magic number
-  int part = 5;
+  int part = 1;
 
-  Eigen::Vector3d vec = p1 - p0;
+  Eigen::Vector3d vec = (p1 - p0) / part;
   Eigen::Vector3d u, v;
   u = Eigen::Vector3d(0, 1, vec(1) / vec(2)).normalized();
   v = u.cross(vec).normalized();
 
+  V.resize(4 * (part + 1), 3);
+  Eigen::Vector3d points[] = {p0, p0 + u, p0 + v, p0 + u + v};
 
+  // Generating vertices
+  for (int i = 0; i < part + 1; i++) {
+    for (int j = 0; j < 4; j++) {
+      V.row(4 * i + j) = points[j] + i * vec;
+    }
+  }
 
+  F.resize(part * 8 + 4, 3);
+  // Generating faces
+  for (int i = 0; i < part; i++) {
+    for (int j = 0; j < 4; j++) {
+      int x = j;
+      int y = (j + 1) % 4;
+      F.row(8 * i + 2 * j + 0) = Eigen::Vector3i(4 * (i + 1) + x, 4 * i + y, 4 * i + x);
+      F.row(8 * i + 2 * j + 1) = Eigen::Vector3i(4 * (i + 1) + x, 4 * (i + 1) + y, 4 * i + y);
+    }
+  }
+
+  F.row(part * 8 + 0) = Eigen::Vector3i(0, 1, 2);
+  F.row(part * 8 + 1) = Eigen::Vector3i(2, 3, 0);
+  F.row(part * 8 + 2) = Eigen::Vector3i(4 * part + 2, 4 * part + 1, 4 * part + 0);
+  F.row(part * 8 + 3) = Eigen::Vector3i(4 * part + 0, 4 * part + 3, 4 * part + 2);
 }
