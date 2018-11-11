402a36f151abfbd9943fac85f80c269b3e9b72bb
bickybilly <bilbilv@mcmaster.ca>
2018-11-11 14:19:46 -0800

    User interaction for bezier points

--- main.cpp
+++ main.cpp
@@ -15,22 +15,7 @@ int main(int argc, char *argv[])
 {
   std::vector<Eigen::MatrixXd> VV;
   std::vector<Eigen::MatrixXi> FF;
-  // Load in a mesh
-//  igl::read_triangle_mesh(argc>1 ? argv[1] : "../shared/data/knight.off", V, F);
-
-//
-//  Eigen::MatrixXi E = edges(F);
-//  int Chi = euler_characteristic(F);
-//  std::cout<<"Edge list E is "<<E.rows()<<"x"<<E.cols()<<std::endl;
-//  std::cout<<"Euler Characteristic: "<<Chi<<std::endl;
-
-  // OUR CODE HERE!
-  //generate_bone(Eigen::Vector3d(0, 0, 0), Eigen::Vector3d(1, 3, 5), V, F);
-
-  Eigen::MatrixXd B;
-  bezier(Eigen::Vector3d(0, 0, 0), Eigen::Vector3d(0, 1, 2.5), Eigen::Vector3d(0, 0, 5), 10, B);
-
-  std::cout << B << std::endl;
+  std::vector<Eigen::MatrixXd> BB;
 
   // Create a libigl Viewer object 
   igl::opengl::glfw::Viewer viewer;
@@ -41,18 +26,27 @@ int main(int argc, char *argv[])
   const Eigen::RowVector3d blue(0.2,0.3,0.8);
   const Eigen::RowVector3d green(0.2,0.6,0.3);
 
+  // Mode consts
+  enum Mode
+  {
+    NONE = 0,
+    BONE = 1,
+    CURVE = 2,
+  } mode = CURVE;
+
   // User input data
   Eigen::Vector3d last_mouse;
   Eigen::MatrixXd bone_points;
+  Eigen::MatrixXd curve_points;
   int n_bp = 0;
-  bool creating_bones = true; //default
+  int n_cp = 0;
 
   // Define update functions for viewer
   const auto & update = [&]()
   {
-    if (creating_bones) {
+    if (mode == BONE) {
       viewer.data().clear();
-      viewer.data().set_points(bone_points,orange);
+      viewer.data().set_points(bone_points, orange);
       // Setting multiple meshes
       // http://www.alecjacobson.com/weblog/?p=4679
       if (VV.size() > 0) {
@@ -62,14 +56,18 @@ int main(int argc, char *argv[])
         viewer.data().set_mesh(V,F);
       }
     }
+    else if (mode == CURVE) {
+      viewer.data().set_points(curve_points, blue);
+    }
   };
 
   viewer.callback_mouse_down = 
     [&](igl::opengl::glfw::Viewer&, int, int)->bool
   {
-    last_mouse = Eigen::Vector3d(viewer.current_mouse_x,viewer.core.viewport(3)-viewer.current_mouse_y,0);
-    if (creating_bones && (n_bp < 2)) {
+    if (mode > 0) {
+      std::cout << "mode: " << mode << std::endl;
       // Ray cast to x-y plane
+      last_mouse = Eigen::Vector3d(viewer.current_mouse_x,viewer.core.viewport(3)-viewer.current_mouse_y,0);
       Eigen::Vector3d source, dir, intersection;
       Eigen::Vector2d p;
       p << last_mouse.head(2)(0), last_mouse.head(2)(1);
@@ -84,29 +82,46 @@ int main(int argc, char *argv[])
                           source,
                           dir,
                           intersection);
-      // Add intersection to list of bone points
-      bone_points.conservativeResize(bone_points.rows() + 1, 3);
-      bone_points.row(bone_points.rows() - 1) = intersection;
-      n_bp++;
-      // For every 2 points, generate a bone
-      if (n_bp == 2){
-        std::cout << "GEN BONE!" << std::endl;
-        Eigen::MatrixXd V;
-        Eigen::MatrixXi F;
-        Eigen::Vector3d p1 = bone_points.row(bone_points.rows() - 1);
-        Eigen::Vector3d p2 = bone_points.row(bone_points.rows() - 2);
-        generate_bone(p1, p2, V, F);
-        std::cout << "all bone points: \n" << bone_points << std::endl;
-        std::cout << "new V: \n" << V << std::endl;
-        // Add to list of meshes
-        VV.push_back(V);
-        FF.push_back(F);
-        std::cout << "num meshes: \n" << VV.size() << std::endl;
-        n_bp = 0;
+      if (mode == BONE) {
+        // Add intersection to list of bone points
+        bone_points.conservativeResize(bone_points.rows() + 1, 3);
+        bone_points.row(bone_points.rows() - 1) = intersection;
+        n_bp++;
+        // For every 2 points, generate a bone
+        if (n_bp == 2){
+          Eigen::MatrixXd V;
+          Eigen::MatrixXi F;
+          Eigen::Vector3d p1 = bone_points.row(bone_points.rows() - 1);
+          Eigen::Vector3d p2 = bone_points.row(bone_points.rows() - 2);
+          generate_bone(p1, p2, V, F);
+          // Add to list of meshes
+          VV.push_back(V);
+          FF.push_back(F);
+          n_bp = 0;
+        }
       }
-      update();
-      return true;
-    }
+        else if (mode == CURVE) {
+          // Add intersection to list of curve points
+          std::cout << "curve?" << std::endl;
+          curve_points.conservativeResize(curve_points.rows() + 1, 3);
+          curve_points.row(curve_points.rows() - 1) = intersection;
+          std::cout << curve_points << std::endl;
+          n_cp++;
+          // For every 3 points, generate a curve
+          if (n_cp == 3) {
+            Eigen::Vector3d p1 = curve_points.row(curve_points.rows() - 1);
+            Eigen::Vector3d p2 = curve_points.row(curve_points.rows() - 2);
+            Eigen::Vector3d p3 = curve_points.row(curve_points.rows() - 3);
+            Eigen::MatrixXd B;
+            bezier(p1, p2, p3, 10, B);
+            BB.push_back(B);
+            n_cp = 0;
+            std::cout << "B \n" << B << std::endl;
+          }
+        }
+        update();
+        return true;
+      }
     return false;
   };
 
@@ -117,12 +132,17 @@ int main(int argc, char *argv[])
     switch(key) {
       case 'b':
       {
-        creating_bones = true;
+        mode = BONE;
+        break;
+      }
+      case 'c':
+      {
+        mode = CURVE;
         break;
       }
       case ' ':
       {
-        creating_bones = false;
+        mode = NONE;
         break;
       }
     }
