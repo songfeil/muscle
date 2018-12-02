8f0d81285bee98ed761b325c3f159ed786cfe0bc
bickybilly <bilbilv@mcmaster.ca>
2018-12-01 19:47:57 -0500

    Organized main a bit

--- main.cpp
+++ main.cpp
@@ -47,11 +47,19 @@ Mode mode = NONE;
 Eigen::Vector3d last_mouse;
 Eigen::MatrixXd control_points;
 int n_points = 0;
+bool mouse_down = false;
 
 Eigen::MatrixXd Vbone;
 Eigen::MatrixXi Fbone;
 Eigen::Vector3d bp0;
 
+void intersection_with_xy_plane(const igl::opengl::glfw::Viewer & viewer, Eigen::Vector3d & intersection) {
+  Eigen::Vector3d source, dir;
+  Eigen::Vector2d p;
+  p << last_mouse.head(2)(0), last_mouse.head(2)(1);
+  igl::unproject_ray(p, viewer.core.view, viewer.core.proj, viewer.core.viewport, source, dir);
+  ray_intersect_plane(Eigen::Vector3d(0, 0, 0), Eigen::Vector3d(0, 0, 1), source, dir, intersection);
+}
 
 int main(int argc, char *argv[])
 {
@@ -102,10 +110,11 @@ int main(int argc, char *argv[])
   viewer.callback_mouse_down = 
     [&](igl::opengl::glfw::Viewer&, int, int)->bool
   {
+    mouse_down = true;
+    last_mouse = Eigen::Vector3d(viewer.current_mouse_x,viewer.core.viewport(3)-viewer.current_mouse_y,0);
 
     /* Not NONE */
     if (mode > 0) {
-      last_mouse = Eigen::Vector3d(viewer.current_mouse_x,viewer.core.viewport(3)-viewer.current_mouse_y,0);
 
       /* FACE_SELECT */
       if (mode == FACE_SELECT) {
@@ -123,15 +132,13 @@ int main(int argc, char *argv[])
 
       /* Point input modes */
       else {
-        Eigen::Vector3d source, dir, intersection;
-        Eigen::Vector2d p;
-        p << last_mouse.head(2)(0), last_mouse.head(2)(1);
-        igl::unproject_ray(p, viewer.core.view, viewer.core.proj, viewer.core.viewport, source, dir);
-        ray_intersect_plane(Eigen::Vector3d(0, 0, 0), Eigen::Vector3d(0, 0, 1), source, dir, intersection);
+        Eigen::Vector3d intersection;
+        intersection_with_xy_plane(viewer, intersection);
 
         /* BONE */
         if (mode == BONE) {
           
+          // Start of bone
           if (n_points == 0) {
             Eigen::MatrixXd Vnew = Vbone.replicate(1, 1);
             start_flinstone_bone(intersection, Vnew);
@@ -140,6 +147,7 @@ int main(int argc, char *argv[])
             bp0 = intersection;
             n_points++;
           } 
+          // End of bone
           else if (n_points == 1) {
             Eigen::MatrixXd Vnew = Vbone.replicate(1, 1);
             transform_flinstone_bone(bp0, intersection, Vnew);
@@ -172,23 +180,44 @@ int main(int argc, char *argv[])
   viewer.callback_mouse_move = 
     [&](igl::opengl::glfw::Viewer&, int, int)->bool
   {
+    last_mouse = Eigen::Vector3d(viewer.current_mouse_x,viewer.core.viewport(3)-viewer.current_mouse_y,0);
+    // Wiggle around bone after one point has been put down!
     if ((mode == BONE) && (n_points == 1)) {
-      last_mouse = Eigen::Vector3d(viewer.current_mouse_x,viewer.core.viewport(3)-viewer.current_mouse_y,0);
-      Eigen::Vector3d source, dir, intersection;
-      Eigen::Vector2d p;
-      p << last_mouse.head(2)(0), last_mouse.head(2)(1);
-      igl::unproject_ray(p, viewer.core.view, viewer.core.proj, viewer.core.viewport, source, dir);
-      ray_intersect_plane(Eigen::Vector3d(0, 0, 0), Eigen::Vector3d(0, 0, 1), source, dir, intersection);
+      
+      Eigen::Vector3d intersection;
+      intersection_with_xy_plane(viewer, intersection);
 
       Eigen::MatrixXd Vnew = Vbone.replicate(1, 1);
       transform_flinstone_bone(bp0, intersection, Vnew);
       VV.pop_back();
       VV.push_back(Vnew);
+
+      update();
+      return true;
+    }
+    // Continuous "sketching" input for muscle points
+    else if (mouse_down && (mode == MUSCLE)) {
+      
+      Eigen::Vector3d intersection;
+      intersection_with_xy_plane(viewer, intersection);
+
+      control_points.conservativeResize(control_points.rows() + 1, 3);
+      control_points.row(control_points.rows() - 1) = intersection;
+      n_points++;
+
       update();
       return true;
     }
     return false;
   };
+
+  /* Mouse up */
+  viewer.callback_mouse_up = 
+    [&](igl::opengl::glfw::Viewer&, int, int)->bool
+    {
+      mouse_down = false;
+      return false;
+    };
 
   /* Key presses */
   viewer.callback_key_pressed = 
@@ -198,6 +227,7 @@ int main(int argc, char *argv[])
       case 'B':
       case 'b':
       {
+        // Bone input mode
          mode = BONE;
          control_points.resize(0, 3);
          n_points = 0;
@@ -206,6 +236,7 @@ int main(int argc, char *argv[])
       case 'M':
       case 'm':
       {
+        // Muscle input mode
          mode = MUSCLE;
          control_points.resize(0, 3);
          n_points = 0;
@@ -213,17 +244,20 @@ int main(int argc, char *argv[])
       }
       case ' ':
       {
-         mode = NONE;
+        // Panning/scene browsing
+        mode = NONE;
         break;
       }
       case 'f':
       {
-         mode = FACE_SELECT;
+        // Face selecting mode
+        mode = FACE_SELECT;
         break;
       }
       case 'G':
       case 'g':
       {
+        // Generate muscle from points
         if ( mode == MUSCLE) {
           generate_muscle(control_points, n_points, V, F, selected_faces, VV, FF);
           control_points.resize(0, 3);
@@ -233,16 +267,17 @@ int main(int argc, char *argv[])
       }
       case 'i':
       {
-        Eigen::MatrixXd Vnew;
-        Eigen::MatrixXi Fnew;
-        Eigen::MatrixXd muscle_mesh = VV.back(); // Assuming muscle was last mesh added
-        int start = V.rows() - muscle_mesh.rows();
-        int end = V.rows();
-        //inflate_muscle(V, F, start, end, Vnew);
-        VV.pop_back();
-        VV.push_back(Vnew);
-        std::cout << "INFLATE" << std::endl;
-        break;
+        // // Inflate muscle mesh
+        // Eigen::MatrixXd Vnew;
+        // Eigen::MatrixXi Fnew;
+        // Eigen::MatrixXd muscle_mesh = VV.back(); // Assuming muscle was last mesh added
+        // int start = V.rows() - muscle_mesh.rows();
+        // int end = V.rows();
+        // inflate_muscle(V, F, start, end, Vnew);
+        // VV.pop_back();
+        // VV.push_back(Vnew);
+        // std::cout << "INFLATE" << std::endl;
+        // break;
       }
       case 's':
       {
